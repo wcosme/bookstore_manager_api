140d616a29e715ce1f9831415bc61868a5a211de
Wallace Gonçalves <wallacecosme@gmail.com>
2021-03-24 22:39:44 -0300

    Configuration sweggar created

--- bookstoremanager/src/main/java/com/wg/bookstoremanager/controller/BookController.java
+++ bookstoremanager/src/main/java/com/wg/bookstoremanager/controller/BookController.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
 	
+	@ApiOperation(value = "Return an example hello world.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Sucess method return")
+	})
 	@GetMapping
 	public String hello() {		
 		return "Helo Bookstore Manager";
