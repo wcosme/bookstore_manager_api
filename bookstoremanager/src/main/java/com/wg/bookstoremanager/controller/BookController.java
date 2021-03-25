ba1abba517dd5f9d1e5fd9522e31735bf6d33d4d
Wallace Gonçalves <wallacecosme@gmail.com>
2021-03-24 22:03:22 -0300

    First commit

---
+++ bookstoremanager/src/main/java/com/wg/bookstoremanager/controller/BookController.java
@@ -0,0 +1,17 @@
+package com.wg.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+	
+	@GetMapping
+	public String hello() {		
+		return "Helo Bookstore Manager";
+	}
+
+}
